6a3eeee529d8adb8089847fc0be97eabc49d5637
Bradley Ryan <brad.conl3y@gmail.com>
2017-07-26 18:01:25 -0400

    added functionality to switch unchecked to checked

--- app.js
+++ app.js
@@ -3,11 +3,25 @@ let existingToDos = JSON.parse(fs.readFileSync('./todos.json', 'utf8'));
 let isChecked
 let id
 
-const addToDo = (body, isChecked = false, id = existingToDos.todos.length + 1) => {
+const writeTodos = () => {
+  fs.writeFileSync('./todos.json', JSON.stringify(existingToDos))
+}
+
+const addToDo = (body, isChecked = "unchecked", id = existingToDos.todos.length + 1) => {
   let todo = { body, isChecked, id }
   existingToDos.todos.push(todo);
-  fs.writeFileSync('./todos.json', JSON.stringify(existingToDos))
-};
 
+  writeTodos()
+}
 
-module.exports = { addToDo, existingToDos }
+const readTodos = () => JSON.parse(fs.readFileSync('./todos.json').toString())
+
+const toggleTodo = (todoId) => {
+  let currentTodo = existingToDos.todos.find(todo => todo.id === todoId)
+
+  currentTodo.isChecked = currentTodo.isChecked === "checked" ? "unchecked" : "checked"
+
+  writeTodos()
+}
+
+module.exports = { addToDo, existingToDos, readTodos, toggleTodo }
--- express-app.js
+++ express-app.js
@@ -1,10 +1,9 @@
 const express = require('express')
 const mustacheExpress = require('mustache-express')
 const bodyParser = require('body-parser')
-const addToDo = require('./app')
-const existingToDos = require('./todos.json')
+const appHelper = require('./app')
 const app = express()
-// const todos = require('./todos.js')
+const fs = require('fs')
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
@@ -17,15 +16,21 @@ app.set('views', './templates')
 app.set('view engine', 'mst')
 
 app.get('/', (request, response) => {
-  response.render('index', existingToDos)
+  response.render('index', appHelper.readTodos())
 })
 
-app.post('/', (request, response) => {
+app.post('/create', (request, response) => {
   const toDoItem = request.body.todo
-  addToDo.addToDo(toDoItem)
-  response.render("index", existingToDos)
+  appHelper.addToDo(toDoItem)
+  response.redirect('/')
+})
+
+app.post('/complete/:id', (request, response) => {
+  const todoId = parseInt(request.params.id)
+  appHelper.toggleTodo(todoId)
+  response.redirect('/')
 })
 
 app.listen(3000, () => {
-  console.log('All your GET requests are belong to me - on port 3000')
+  console.log('All your requests are belong to us - on port 3000')
 })
